434aeef34b56713ed0d22caa850258d2f8ee6f40
Rimi Reza (Duity) <duity@therapservices.net>
2025-01-14 12:19:11 +0600

    Added react job app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+    </>
+  )
+}
+
+export default App
