b2b809eb08f88b50bfe2de86e0c25165b810a684
Rimi Reza <rimi.duity22@gmail.com>
2025-01-15 13:20:40 +0600

    Removed useEffect hook & used useDataLoader from react-router-dom to load data

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 const App = () => {
 
@@ -19,7 +19,7 @@ const App = () => {
       <Route path='/' element={<MainLayout/>}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/jobs/:id' element={ <JobPage />} />
+        <Route path='/jobs/:id' element={ <JobPage />} loader={jobLoader}/>
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,35 +1,18 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
+import { useLoaderData } from 'react-router-dom';
 
 const JobPage = () => {
-    const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data);
-            } catch {
-                console.log('Error fetching data', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        fetchJob();
-    }, [])
+    const job = useLoaderData();
 
   return (
-    loading ? <Spinner /> 
-    : (
         <h1>{ job.title }</h1>
-    )
   );
 }
 
-export default JobPage
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = res.json();
+    return data;
+};
+
+export { JobPage as default, jobLoader }
